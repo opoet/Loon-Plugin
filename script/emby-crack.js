c3d5f1e40d8b3e34c848c26663f7c9ebcb9089bc
opoet <opoet@qq.com>
2023-03-26 01:08:22 +0800

    fix balabala

--- script/emby-crack.js
+++ script/emby-crack.js
@@ -1,31 +1,41 @@
-function hackEmbyVerifyRes(response, res) {
-  response.statusCode = 200;
-  response.headers['Content-Type'] = 'application/json';
+/*
+  Author: opoet
+  Date: 2023-03-26
+  Description: Emby crack loon script
+*/
+
+function crackEmbyVerifyRes(response, res) {
+  response.headers["Content-Type"] = "application/json; charset=utf-8";
+  response.status = 200;
   response.body = res;
-  console.log('Rewrite Emby verify URL response successfully');
+  console.log("Rewrite Emby verify URL response successfully");
 }
 
 function processResponse(response) {
-  const url = new URL(response.url);
-  const urlPath = url.pathname;
+  const { url } = $request;
+  const regex = /https?:\/\/mb3admin\.com(\/[^?]+)\?/;
+  const match = url.match(regex);
 
-  const rewriteRules = {
-      '/admin/service/registration/validateDevice': '{"cacheExpirationDays":999,"message":"Device Valid","resultCode":"GOOD"}',
-      '/admin/service/registration/validate': '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}',
-      '/admin/service/registration/getStatus': '{planType: "Lifetime", deviceStatus: 0, subscriptions: []}',
-      '/admin/service/appstore/register': '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}',
-      '/emby/Plugins/SecurityInfo': '{SupporterKey: "", IsMBSupporter: true}'
-  };
+  if (match) {
+    const rewriteRules = {
+      "/admin/service/registration/validateDevice":
+        '{"cacheExpirationDays":999,"message":"Device Valid","resultCode":"GOOD"}',
+      "/admin/service/registration/validate":
+        '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}',
+      "/admin/service/registration/getStatus":
+        '{planType: "Lifetime", deviceStatus: 0, subscriptions: []}',
+      "/admin/service/appstore/register":
+        '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}',
+      "/emby/Plugins/SecurityInfo": '{SupporterKey: "", IsMBSupporter: true}',
+    };
 
-  if (rewriteRules.hasOwnProperty(urlPath)) {
-      hackEmbyVerifyRes(response, rewriteRules[urlPath]);
+    const urlPath = match[1];
+
+    if (rewriteRules.hasOwnProperty(urlPath)) {
+      crackEmbyVerifyRes(response, rewriteRules[urlPath]);
+    }
   }
-
   return response;
 }
 
-if (typeof $response !== 'undefined') {
-  $done(processResponse($response));
-} else {
-  $done({});
-}
+$done(processResponse($response));
